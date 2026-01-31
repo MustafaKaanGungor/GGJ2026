f3f8f5317c0554af7db7615c5cc4031ee0942f2d
İsmail Özsaygı <devozsaygi@gmail.com>
2026-01-31 13:05:02 +0300

    Add: Player reference to HUD object

---
+++ Assets/_Scripts/HUD/Scripts/Core/HUD.cs
@@ -0,0 +1,12 @@
+using Player.Core;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace HUD.Core
+{
+    [DisallowMultipleComponent]
+    public class HUD : MonoBehaviour
+    {
+        public PlayerBehaviour Player;
+    }
+}
