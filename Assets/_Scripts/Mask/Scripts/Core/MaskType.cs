ae6b63a202b8b25c9d9432ff6235d6cb6d2fcdab
İsmail Özsaygı <devozsaygi@gmail.com>
2026-01-31 11:15:01 +0300

    Add: Mask types as byte field enums

---
+++ Assets/_Scripts/Mask/Scripts/Core/MaskType.cs
@@ -0,0 +1,15 @@
+// ReSharper disable once CheckNamespace
+
+namespace Mask.Core
+{
+    public enum MaskType : byte
+    {
+        // @formatter:off
+        Default                         = 0,
+        DamageAmplification             = 1,
+        HealthRegenerationAmplification = 2,
+        RangeAmplification              = 3,
+        EnemySpeedCurse                 = 4
+        // @formatter:on
+    }
+}
