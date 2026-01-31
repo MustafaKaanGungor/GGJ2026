cabfba4f3c6c19e8a18d41e8cfd736d35fe6e81e
İsmail Özsaygı <devozsaygi@gmail.com>
2026-01-31 13:00:00 +0300

    Add: Icon view to sprite

--- Assets/_Scripts/HUD/Scripts/Inventory/SlotView.cs
+++ Assets/_Scripts/HUD/Scripts/Inventory/SlotView.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 // ReSharper disable once CheckNamespace
 
@@ -7,5 +8,14 @@ namespace HUD.Inventory
     [DisallowMultipleComponent]
     public class SlotView : MonoBehaviour
     {
+        [SerializeField] private Image iconView;
+        
+        private Sprite _icon;
+
+        private void Start()
+        {
+            TryGetComponent(out iconView);
+            iconView.sprite = _icon;
+        }
     }
 }
